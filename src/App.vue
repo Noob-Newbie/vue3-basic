1bbe115c4ad5b4b252c9696bcf99f696fbf7d418
Noob-Newbie <ygzzang97@gmail.com>
2024-04-09 17:36:38 +0900

    Inflearn Vue3 Basic 04/09

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,71 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h2>{{teacher.name}}</h2>
+    <h3>강의가 있습니까?</h3>
+    <p>{{hasLecture}}</p>
+    <p>{{hasLecture}}</p>
+    <p>{{existLecture()}}</p>
+    <p>{{existLecture()}}</p>
+    <button @click="count++">Counter : {{count}}</button>
+    <h2>이름</h2>
+    <p>{{fullNm}}</p>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import {computed, reactive, ref} from "vue";
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+export default {
+  setup() {
+    const teacher = reactive({
+      name : '테스트',
+      lectures : [
+          'HTML/CSS',
+          'JavaScript',
+          'Vue3'
+      ]
+    })
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    //성능적으로 함수보다 computed사용이 좋음
+    const hasLecture = computed(() => {
+      console.log('computed')
+      return teacher.lectures.length > 0 ? '있음' : '없음'
+    })
+
+    const existLecture = () => {
+      console.log('method')
+      return teacher.lectures.length > 0 ? '있음' : '없음'
+    }
+
+    const count = ref(0)
+
+
+    const fNm = ref('길동')
+    const lNm = ref('홍')
+
+    const fullNm = computed({
+      get() {
+        return fNm.value + lNm.value
+      },
+      set(value){
+        console.log(value)
+      }
+
+    })
+
+    console.log('콘솔 출력' , fullNm.value);
+
+    return {
+      teacher,
+      hasLecture,
+      existLecture,
+      count,
+      fullNm
+    }
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
+</script>
+
+<style lang="scss" scoped>
+
 </style>
